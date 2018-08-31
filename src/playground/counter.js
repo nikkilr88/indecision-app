4d4d0217e95d00c4fef9a5a40db4fd01bc2f7abb
Nicole <nicole.sattler@live.com>
2018-08-31 17:43:12 -0500

    Redo counter exercise with classes

--- src/playground/counter.js
+++ src/playground/counter.js
@@ -1,31 +1,46 @@
-let count = 0
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
 
-const addOne = () => {
-  count++
-  renderCounterApp()
+    this.state = {
+      count: 0
+    }
+
+    this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+  }
+  handleAddOne() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+  handleMinusOne() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count - 1
+      }
+    })
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    )
+  }
 }
 
-const minusOne = () => {
-  count--
-  renderCounterApp()
-}
-
-const reset = () => {
-  count = 0
-  renderCounterApp()
-}
-
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  )
-
-  ReactDOM.render(templateTwo, document.getElementById('root'))
-}
-
-renderCounterApp()
+ReactDOM.render(<Counter />, document.getElementById('root'))
