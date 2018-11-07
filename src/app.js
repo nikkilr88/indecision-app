777643805536d6aafaad5a5f7a09185d52cf8447
Nicole <nicole.sattler@live.com>
2018-11-07 17:28:15 -0600

    Import components

--- src/app.js
+++ src/app.js
@@ -1,10 +1,104 @@
-// import { square, add } from './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// console.log('app.js is running...')
-// console.log(square(2))
-// console.log(add(5, 5))
+// Components
+import AddOption from './components/AddOption'
+import Options from './components/Options'
+import Action from './components/Action'
+import Header from './components/Header'
 
-// import { isAdult, canDrink } from './person'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      options: props.options
+    }
 
-// console.log(isAdult(17))
-// console.log(canDrink(20))
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
+    this.handlePick = this.handlePick.bind(this)
+    this.handleAddOption = this.handleAddOption.bind(this)
+    this.handleDeleteOption = this.handleDeleteOption.bind(this)
+  }
+
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+
+      if (options) {
+        this.setState(() => ({ options }))
+      }
+    } catch (error) {
+      //Do nothing at all
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length === this.state.options.length) return
+
+    const json = JSON.stringify(this.state.options)
+    localStorage.setItem('options', json)
+  }
+
+  componentWillUnmount() {
+    console.log('Component unmounted')
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => ({
+      options: []
+    }))
+  }
+
+  handleDeleteOption(option) {
+    this.setState(prevState => ({
+      options: prevState.options.filter(prevOption => prevOption != option)
+    }))
+  }
+
+  handlePick() {
+    let rand = Math.floor(Math.random() * this.state.options.length)
+    let pick = this.state.options[rand]
+
+    alert(pick)
+  }
+
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This options already exists'
+    }
+
+    this.setState(prevState => ({
+      options: prevState.options.concat(option)
+    }))
+  }
+
+  render() {
+    const title = 'Indecision App'
+    const subtitle = 'Put your life in the hands of a computer...'
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOption={this.handleDeleteOption}
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+        <AddOption handleAddOption={this.handleAddOption} />
+      </div>
+    )
+  }
+}
+
+App.defaultProps = {
+  options: []
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
