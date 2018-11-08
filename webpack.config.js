1da7f7af5ce6df0972a69c4b22093656289ac460
Nicole <nicole.sattler@live.com>
2018-11-08 12:49:23 -0600

    Set up dev config

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,15 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ]
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
   }
 }
 
