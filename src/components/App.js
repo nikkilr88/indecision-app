83af52be842c30baff65a54acb7c5d2111f8e62d
Nicole <nicole.sattler@live.com>
2018-11-14 13:32:23 -0600

    Add container

--- src/components/App.js
+++ src/components/App.js
@@ -80,16 +80,19 @@ class App extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOption={this.handleDeleteOption}
-          handleDeleteOptions={this.handleDeleteOptions}
-        />
-        <AddOption handleAddOption={this.handleAddOption} />
+
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            handleDeleteOption={this.handleDeleteOption}
+            handleDeleteOptions={this.handleDeleteOptions}
+          />
+          <AddOption handleAddOption={this.handleAddOption} />
+        </div>
 
         <OptionModal
           selectedOption={this.state.selectedOption}
