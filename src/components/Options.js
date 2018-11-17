9a4c51d440814170f21d97409e1b899d93aef0a8
Nicole <nicole.sattler@live.com>
2018-11-17 13:04:05 -0600

    Add wrappers and option count

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,6 +6,7 @@ const Options = props => {
   const optionsList = props.options.map((option, i) => (
     <Option
       key={i}
+      count={i + 1}
       optionText={option}
       handleDeleteOption={props.handleDeleteOption}
     />
@@ -13,13 +14,19 @@ const Options = props => {
 
   return (
     <div>
-      <button
-        className="button button--link"
-        onClick={props.handleDeleteOptions}
-      >
-        Remove all
-      </button>
-      {props.options.length === 0 && <p>No items</p>}
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+        <button
+          className="button button--link"
+          onClick={props.handleDeleteOptions}
+        >
+          Remove all
+        </button>
+      </div>
+
+      {props.options.length === 0 && (
+        <p className="widget__message">Add an item to get started</p>
+      )}
       {optionsList}
     </div>
   )
